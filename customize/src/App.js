106f5ecced56a8299735adfa56b52739540dc51a
Desknik <106723609+Desknik@users.noreply.github.com>
2023-02-20 16:55:30 -0300

    Create React App
    Criado o projeto com React.js e organizado o esquema de pastas

---
+++ customize/src/App.js
@@ -0,0 +1,14 @@
+import './styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Customize - Quadros</h1>
+        <p>Create React App/ Esquema de pastas</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
